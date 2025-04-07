9bf32e0bdf4529932859785dcc4f762ce59ecd81
ytower <ytower@proton.me>
2025-04-06 21:20:25 -0300

    feat: libft && mlx

---
+++ includes/fractol.h
@@ -0,0 +1,10 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdlib.h>
+# include "../libs/libft/includes/libft.h"
+# include "../libs/minilibx/mlx.h"
+
+// ---
+
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "../includes/fractol.h"
+
+int	main(int argc, char **argv)
+{
+	ft_printf("hello, world!\n");
+	return (0);
+}
+
